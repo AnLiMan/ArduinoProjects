495cce7aba2ee5efe74ec19fc23770d4950de58a
Andrey Lisov <42667249+AnLiMan@users.noreply.github.com>
2021-09-09 13:24:02 +0500

    Add files via upload

---
+++ 2.2._LED_Blink/2.2._LED_Blink.ino
@@ -0,0 +1,18 @@
+#define LED1 6 //Пин светодиода 1
+
+void setup() {
+  pinMode(LED1, OUTPUT);  // Настройка пина на выход
+}
+
+void loop() {
+  for (int i = 100; i <= 1000; i+=100) {
+    digitalWrite(LED1, HIGH);   //Устанавливаем пин на высокое напряжение
+    delay(i); 
+    digitalWrite(LED1, LOW);    //Устанавливаем пин на низкое напряжение
+    delay(i);
+
+    if (i > 1000) {
+      i = 100;
+    }
+  }
+}
