4344805e835493a7856917bcd0f0e976f2b09551
Andrey Lisov <42667249+AnLiMan@users.noreply.github.com>
2021-09-10 11:49:56 +0500

    Add files via upload

---
+++ 3.2.Temperature/3.2.Temperature.ino
@@ -0,0 +1,38 @@
+#define LED_BLUE 9 //Пин катода синего светодиода
+#define LED_GREEN 10 //Пин катода зелёного светодиода
+#define LED_RED 11 //Пин катода красного светодиода
+
+#define TEMP A0 // Подключаем вывод датчика температуры к контакту 
+int LOWER_BOUND = 139; // Нижнее пороговое значение
+int UPPER_BOUND = 147; // Верхнее пороговое значение
+int val = 0; // Переменная для хранения аналогового значения
+
+void setup() {
+  pinMode(LED_BLUE, OUTPUT);
+  pinMode(LED_GREEN, OUTPUT);
+  pinMode(LED_RED, OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  val = analogRead(TEMP);
+  Serial.println(val);
+  // Синий
+  if (val < LOWER_BOUND) {
+    digitalWrite(LED_RED, HIGH);
+    digitalWrite(LED_GREEN, HIGH);
+    digitalWrite(LED_BLUE, LOW);
+  }
+  // Красный
+  if (val > UPPER_BOUND) {
+    digitalWrite(LED_RED, LOW);
+    digitalWrite(LED_GREEN, HIGH);
+    digitalWrite(LED_BLUE, HIGH);
+  }
+  //Зелёный
+  else {
+    digitalWrite(LED_RED, HIGH);
+    digitalWrite(LED_GREEN, LOW);
+    digitalWrite(LED_BLUE, HIGH);
+  }
+}
