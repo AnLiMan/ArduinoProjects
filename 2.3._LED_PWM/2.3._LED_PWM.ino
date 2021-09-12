20d51fe2bb445a3c3d3cbd5cc1257083f2e3e36d
Andrey Lisov <42667249+AnLiMan@users.noreply.github.com>
2021-09-12 20:36:17 +0500

    Update 2.3._LED_PWM.ino

--- 2.3._LED_PWM/2.3._LED_PWM.ino
+++ 2.3._LED_PWM/2.3._LED_PWM.ino
@@ -2,18 +2,13 @@
 
 void setup() {
   pinMode(LED1, OUTPUT); // Настройка пинов на выход
-  Serial.begin(9600);
+  Serial.begin(9600); //Откроем последовательный порт для общения Arduino с компьютером
 }
 
 void loop() {
   for (byte i = 0; i <= 255; i++) {
-
-    analogWrite(LED1, i); // Генерирование напряжения на уровне текущей градации
+    analogWrite(LED1, i); // Генерирование скважности на уровне текущей градации
     Serial.println(i);
     delay(75);
-
-    if (i > 255) {
-      i = 0;
-    }
   }
 }
